0c20570c182f196349e1e08d1c84c287ca3782c2
nlowe31 <nlowe@e2r9p7.42.fr>
2017-02-09 17:23:55 +0100

    update

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: nlowe <nlowe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/02 18:13:12 by nlowe             #+#    #+#             */
-/*   Updated: 2017/02/02 18:16:00 by nlowe            ###   ########.fr       */
+/*   Updated: 2017/02/09 16:07:42 by nlowe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,28 @@
 # include <stdarg.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include "libft.h"
+
+# define PRINTF_FLAGS "#0-+ *$L'."
+
+typedef enum	e_bool
+{
+	false = 0,
+	true = 1
+}				t_bool;
+
+typedef struct	s_arg
+{
+	t_bool		pos;
+	t_bool		left;
+	t_bool		force_sign;
+	t_bool		space;
+	t_bool		pound;
+	t_bool		zero;
+	long long	width;
+	long long	precision;
+}				t_arg;
+
+int		ft_printf(const char *format, ...);
 
 #endif
--- main.c
+++ main.c
@@ -1,50 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nlowe <nlowe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/02 18:02:15 by nlowe             #+#    #+#             */
-/*   Updated: 2017/02/09 16:45:53 by nlowe            ###   ########.fr       */
+/*   Created: 2017/02/03 12:51:25 by nlowe             #+#    #+#             */
+/*   Updated: 2017/02/09 16:07:38 by nlowe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "ft_printf.h"
-
-int		count_args(const char *format)
-{
-	int		i;
-	int		count;
-
-	i = -1;
-	count = 0;
-	while (format[++i])
-		if (format[i] == '%')
-			if (format[i + 1] != '%')
-				count++;
-	return (count);
-}
-
-int		is_flag(const char c)
-{
-	if (ft_strchr(PRINTF_FLAGS, c))
-		return (1);
-	return (0);
-}
-
-int		ft_printf(const char *format, ...)
+/*
+void	print_args(int ac, ...)
 {
 	va_list		args;
 	int			i;
-	int			count;
 
-	count = count_args(format);
-	va_start(args, format);
-	while (i < count)
+	va_start(args, ac);
+	i = 0;
+	while (i < ac)
 	{
-
+		ft_putstr(va_arg(args, char *));
+		ft_putchar('\n');
+		i++;
 	}
 	va_end(args);
+}
+*/
+int		main(void)
+{
+//	print_args(3, "dogs", "cats", "monkeys");
+	// printf("%S\n", "dogs");
+	t_bool		test;
+
+	ft_putnbr(test);
+	if (test)
+		ft_putendl("yes");
+	if (!test)
+		ft_putendl("no");
+
+	//ft_printf("%s%%%ddogs");
 	return (0);
 }
