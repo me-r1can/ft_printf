0315f302426a8ab6d73bcdaf06657869561bfb24
nlowe31 <nlowe@e2r12p6.42.fr>
2017-04-10 19:00:10 +0200

    all issues resolved

--- src/buffer.c
+++ src/buffer.c
@@ -6,7 +6,7 @@
 /*   By: nlowe <nlowe@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/20 14:05:37 by nlowe             #+#    #+#             */
-/*   Updated: 2017/04/05 18:39:37 by nlowe            ###   ########.fr       */
+/*   Updated: 2017/04/10 18:55:16 by nlowe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		ft_putbuff(t_buff *buffer, void *contents, size_t len)
 	ret = 0;
 	while (ret < len)
 	{
-		if ((buffer->pos) >= (BUFF_SIZE - 1))
+		if ((buffer->pos) >= BUFF_SIZE)
 			ft_flushbuff(buffer);
 		buffer->contents[buffer->pos] = c[ret];
 		++ret;
@@ -32,7 +32,7 @@ int		ft_putbuff(t_buff *buffer, void *contents, size_t len)
 
 void	ft_flushbuff(t_buff *buffer)
 {
-	write(buffer->fd, buffer->contents, buffer->pos + 1);
+	write(buffer->fd, buffer->contents, buffer->pos);
 	buffer->pos = 0;
 	ft_bzero(buffer->contents, BUFF_SIZE);
 }
